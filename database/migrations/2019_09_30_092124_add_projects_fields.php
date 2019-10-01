f42a6ddf40c22e5e6f196a203c3a7aea803b767f
Alessandro Tugnoli <a.tugnoli@area.bo.cnr.it>
2019-10-01 15:24:52 +0200

    fix models, permissions

---
+++ database/migrations/2019_09_30_092124_add_projects_fields.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProjectsFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->string('name')->nullable(false);
+            $table->boolean('active')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->dropColumn('active');
+        });
+    }
+}
