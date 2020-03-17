cd5f71e8a02e09d04a99bda5bc5a76a356cdab0f
Giorgio INKODE <giorgio@inkode.it>
2020-03-17 11:59:09 +0100

    status

---
+++ app/Http/Controllers/Commons/CountryController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Commons;
+
+use App\Models\BaseLightTransformer;
+use App\Models\Country;
+use App\Models\Libraries\DepartmentTransformer;
+use App\Models\Libraries\Library;
+use App\Models\Libraries\LibraryTransformer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class CountryController extends ApiController
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(Country $model, BaseLightTransformer $transformer)
+    {
+        $this->model = $model;
+
+        $this->transformer = $transformer;
+
+        $this->broadcast = false;
+    }
+}
