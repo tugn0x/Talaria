3d101fd01e23fcf6057dfdd8ec27bbd49f1023dd
Alessandro Tugnoli <a.tugnoli@area.bo.cnr.it>
2020-04-15 15:42:18 +0200

    User_Tables update migration

---
+++ database/migrations/2020_04_15_132351_update_library_user.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateLibraryUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       
+        Schema::table('library_user', function (Blueprint $table) {            
+            $table->string('user_referent',50)->nullable()->before('status');
+            $table->string('user_mat',20)->nullable()->before('status');
+            $table->string('user_service_phone')->nullable()->before('status');
+            $table->string('user_service_email')->nullable()->before('status');
+            $table->boolean('preferred')->before('status');
+            $table->string('label',20)->before('status');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('library_user', function (Blueprint $table) {            
+            $table->dropColumn('user_referent');
+            $table->dropColumn('user_mat');
+            $table->dropColumn('user_service_phone');
+            $table->dropColumn('user_service_email');
+            $table->dropColumn('preferred');
+            $table->dropColumn('label');
+            
+        });
+    }
+}
