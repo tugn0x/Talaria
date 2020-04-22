7863146da65f7c94d8ad032759067d97bd1254a3
Jacopo <jacomig@gmail.com>
2020-04-22 16:41:49 +0200

    commento

--- frontend/app/containers/Library/UserPage/fields.js
+++ frontend/app/containers/Library/UserPage/fields.js
@@ -50,6 +50,8 @@ export const fields = {
         width: "col-md-6",
         options: [
             /* NOTA: mi piacerebbe prendere queste stinghe da app.global.PatronStatus.xxx (definite in it.json) */
+            /* se scrivi la label con una app.global.variabile verra' tradotta, sempre che app.global.variabile esista nelle traduzioni */ 
+            /* altrimenti legge la label come stringa */
             { value: 2, label: 'app.global.pending' },
             { value: 1, label: 'app.global.enabled' },
             { value: 0, label: 'app.global.disabled' },
