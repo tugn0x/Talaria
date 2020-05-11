b713444eac77c13a90ef6fc4c69b64d19f885f82
Alessandro Tugnoli <a.tugnoli@area.bo.cnr.it>
2020-05-12 00:30:02 +0200

    Catalog Controller fix

--- app/Http/Controllers/Libraries/CatalogController.php
+++ app/Http/Controllers/Libraries/CatalogController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\Libraries;
 
 use App\Http\Controllers\ApiController;
-use App\Models\BaseLightTransformer;
+use App\Models\BaseTransformer;
 use App\Models\Libraries\Catalog;
 use Illuminate\Http\Request;
 
 class CatalogController extends ApiController
 {
-    public function __construct(Catalog $model, BaseLightTransformer $transformer)
+    public function __construct(Catalog $model, BaseTransformer $transformer)
     {
         $this->model = $model;
 
