e08e1771ef8c685c459ea14fb182bfde1905d432
Jacopo <jacomig@gmail.com>
2020-06-19 12:43:37 +0200

    Fix babel-chain compile error

--- frontend/app/components/Form/CustomForm/selectFieldsGroups.js
+++ frontend/app/components/Form/CustomForm/selectFieldsGroups.js
@@ -1,24 +1,44 @@
-import { chain, sortBy } from 'lodash';
+import { sortBy, groupBy } from 'lodash';
 
 /** ***************** SELECTORS **********************/
 
 export const selectFieldsGroups = (fields, fieldsGroups) => {
-    const myFields = []
+    
+    if(Object.keys(fields).length <= 0){ 
+        return
+    }
+    
+    const fieldsArray = []
+    
+    // Trasforma i fields in Array
     Object.keys(fields).map(key => {
         return (
-            myFields.push(fields[key])
+            fieldsArray.push(fields[key])
         )
     })
-    if (myFields.length === 0) {
-        return [];
-    }
-    // groupBy
-    return chain(myFields)
+    
+    // Crea un oggetto con i gruppi. Ogni gruppo un array
+    let fieldsGroupsArray = groupBy(fieldsArray, 'group')
+    
+    // Ordina i fields a seconda dei gruppi e restituscimi un array.
+    // [{info_generali: {}}, {info_amministrative: {}}, ... ]
+    const orderedFieldGroups = []
+    Object.keys(fieldsGroupsArray).map(group => {
+        const label = fieldsGroups[group] ? fieldsGroups[group].label : ''
+        orderedFieldGroups.push({ name: group, label: label, fields: sortBy([...fieldsGroupsArray[group]],'order','asc') })
+    })
+     
+    return orderedFieldGroups
+    
+
+    /*
+    return chain(fieldsArray)
       .groupBy('group').map((group, key) => {
         const label = fieldsGroups[key] ? fieldsGroups[key].label : ''
         const outputGroup = { name: key, label: label, fields: sortBy([...group],'order','asc') }
+       
         return outputGroup
-      }).value()
+      }).value() */
   };
   
   
