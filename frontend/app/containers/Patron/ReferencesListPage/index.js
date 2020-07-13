a465d55ebc7b7dc0057faa18d1a8a545f4e993b0
Alessandro Tugnoli <a.tugnoli@area.bo.cnr.it>
2020-07-13 16:30:12 +0200

    ReferenceList: call applyLabels/groups to selected references

--- frontend/app/containers/Patron/ReferencesListPage/index.js
+++ frontend/app/containers/Patron/ReferencesListPage/index.js
@@ -57,14 +57,14 @@ const ReferencesListPage = (props) => {
          //Dati di test per provare la API
          let TESTlabelIds= [7,5,'ciaoooo'];
          let TESTrefIds=[48,46,44];
-        dispatch(requestApplyLabelsToReferences([labelIds],refIds,intl.formatMessage(messages.addedMessage)))
+        dispatch(requestApplyLabelsToReferences(refIds,[labelIds],intl.formatMessage(messages.addedMessage)))
      }
 
      const applyGroupsToReferences = (groupIds,refIds) => {
          //Dati di test per provare la API
          let TESTgroupIds= [14,15,'testgroup'];
          let TESTrefIds=[48,46,44];
-        dispatch(requestApplyGroupsToReferences([groupIds],refIds,intl.formatMessage(messages.addedMessage)))
+        dispatch(requestApplyGroupsToReferences(refIds,[groupIds],intl.formatMessage(messages.addedMessage)))
     }
 
     return (
