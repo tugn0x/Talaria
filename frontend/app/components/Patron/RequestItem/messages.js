d1c178e2197ad4f2fcefcdab9f4fa46092788456
Alessandro Tugnoli <a.tugnoli@area.bo.cnr.it>
2020-07-21 00:44:22 +0200

    RequestItem status messages

--- frontend/app/components/Patron/RequestItem/messages.js
+++ frontend/app/components/Patron/RequestItem/messages.js
@@ -16,9 +16,37 @@ export default defineMessages({
     id: `${scope}.received`,
     defaultMessage: 'Received',
   },
+  fileReceived: {
+    id: `${scope}.fileReceived`,
+    defaultMessage: 'File Received',
+  },
   notReceived: {
     id: `${scope}.notReceived`,
     defaultMessage: 'Not Received',
   },
+  userAskCancel: {
+    id: `${scope}.userAskCancel`,
+    defaultMessage: 'Cancel requested',
+  },
+  canceled: {
+    id: `${scope}.canceled`,
+    defaultMessage: 'Canceled',
+  },
+  waitingForCost: {
+    id: `${scope}.waitingForCost`,
+    defaultMessage: 'Cost approval requested',
+  },
+  costAccepted: {
+    id: `${scope}.costAccepted`,
+    defaultMessage: 'Cost accepted',
+  },
+  costNotAccepted: {
+    id: `${scope}.costNotAccepted`,
+    defaultMessage: 'Cost not accepted',
+  },
+  readyToDelivery: {
+    id: `${scope}.readyToDelivery`,
+    defaultMessage: 'Ready to delivery',
+  },  
 });
 
