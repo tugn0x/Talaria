713ed79763a28f50829106755ddee8a3814fd5bd
Alessandro Tugnoli <a.tugnoli@area.bo.cnr.it>
2020-08-15 01:44:20 +0200

    formatDateTime util

--- frontend/app/utils/dates.js
+++ frontend/app/utils/dates.js
@@ -3,10 +3,22 @@ import moment from "moment";
 export function formatDate(date, type) {
   switch(type){
     case 'en':
-      return moment(date).format('YYYY-MM-DD hh:mm:ss');  
+      return moment(date).format('YYYY-MM-DD');  
     break;
     case 'it':
       return moment(date).format('DD/MM/YYYY');  
+    break;
+    default:  return moment(date).format('YYYY-MM-DD');
+  }
+}
+
+export function formatDateTime(date, type) {
+  switch(type){
+    case 'en':
+      return moment(date).format('YYYY-MM-DD hh:mm:ss');  
+    break;
+    case 'it':
+      return moment(date).format('DD/MM/YYYY hh:mm:ss');  
     break;
     default:  return moment(date).format('YYYY-MM-DD hh:mm:ss');
   }
