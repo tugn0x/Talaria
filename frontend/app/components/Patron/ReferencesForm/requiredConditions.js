71ca26898fa622394e16c278018c147d2088e0b6
Alessandro Tugnoli <a.tugnoli@area.bo.cnr.it>
2020-09-18 10:39:25 +0200

    Reference Form validation rules

--- frontend/app/components/Patron/ReferencesForm/requiredConditions.js
+++ frontend/app/components/Patron/ReferencesForm/requiredConditions.js
@@ -1,11 +1,12 @@
 export const requiredConditions = (formData) => {
-    const pubyear = formData.pubyear === null ? "" : formData.pubyear;
-    const page_start = formData.page_start === null ? "" : formData.page_start;
-    const first_author = formData.first_author === null ? "" : formData.first_author;
-    const volume = formData.volume === null ? "" : formData.volume;
+    const pubyear = (!formData.pubyear || formData.pubyear === null) ? "" : formData.pubyear;
+    const page_start = (!formData.page_start ||formData.page_start === null) ? "" : formData.page_start;
+    const first_author = (!formData.first_author ||formData.first_author === null) ? "" : formData.first_author;
+    const volume = (!formData.volume || formData.volume === null) ? "" : formData.volume;
     let required = true;
-    
+
     if(pubyear !== "" &&  volume === ""){
+        
         if(page_start === "" && first_author === ""){
             required = true;
         }else if(page_start !== "" || first_author !== ""){
