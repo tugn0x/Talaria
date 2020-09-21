d62ded64dcb4aa33e16f07e32c7aeea98b16bf92
Alessandro Tugnoli <a.tugnoli@area.bo.cnr.it>
2020-09-21 09:04:41 +0200

    Department factory fix

--- database/factories/DepartmentFactory.php
+++ database/factories/DepartmentFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Models\Libraries\Department::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'name' => $faker->city,
     ];
 });
 
