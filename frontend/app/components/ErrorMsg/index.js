4d3665aa4cb038a99af572b4747cd1f8585b0c84
Alessandro Tugnoli <a.tugnoli@area.bo.cnr.it>
2020-11-27 15:09:44 +0100

    ErrorMessage css class fix

--- frontend/app/components/ErrorMsg/index.js
+++ frontend/app/components/ErrorMsg/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 
 function ErrorMsg(props) {
-  const {message}=props;
+  const {message,cssclass}=props;
+
+  let bootstrapCl="alert"
+  if(cssclass)
+    bootstrapCl+=" "+cssclass
+
 
   return (
-    <div className="alert alert-danger" role="alert">
+    <div className={bootstrapCl} role="alert">
     {message}
     </div>
   );
