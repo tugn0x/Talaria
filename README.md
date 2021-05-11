9283a22aa830485f3406c814f2a6c8557957869b
Alessandro Tugnoli <a.tugnoli@area.bo.cnr.it>
2021-05-11 23:25:40 +0200

    Social,Institutional,Spid login configuration in environment

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Install
+1. change settings in `.env.example` and rename it as `.env`
+2. check that your hostname matches `API_DOMAIN` and `FRONTEND_DOMAIN` as it was used for `traefik` load balancer 
+3. run `docker-compose up -d` to start all needed containers
 
-## Laravel
+# Configuration
+## Frontend
+The frontend don't need any configuration; it's based on a nodejs container
+running the application by command `npm start` as the container starts
+
+## Laravel setup 
+(run these from `laravel` container)
 ```bash
 composer install
 php artisan key:generate
--- frontend/app/components/Auth/SocialAuth/index.js
+++ frontend/app/components/Auth/SocialAuth/index.js
@@ -6,15 +6,24 @@ import messages from './messages';
 import { FormattedMessage } from 'react-intl';
 
 export default function Index(props) {
+  const social_enabled=(process.env.SOCIAL_LOGIN && process.env.SOCIAL_LOGIN=="true")?true:false;
+  const institutional_login_enabled=(process.env.INSTITUTIONAL_LOGIN && process.env.INSTITUTIONAL_LOGIN=="true")?true:false;
+  const spid_login_enabled=(process.env.SPID_LOGIN && process.env.SPID_LOGIN=="true")?true:false;
+
+
   return (
     <CardFooter className="p-4">
       <Row>
+        {social_enabled && 
+        <>
         <Col xs="12">
           <Button color="facebook" className="mb-1" block onClick={props.loginFacebook}><span>Facebook</span></Button>
         </Col>
         <Col xs="12">
           <Button color="google-plus" className="mb-1" block onClick={props.loginGoogle}><span>Google</span></Button>
         </Col>
+        </>}
+        {institutional_login_enabled &&         
         <Col xs="12">
           <a className="btn-garr-container" href="https://devnilde.bo.cnr.it/Shibboleth.sso/Login?target=https://devnilde.bo.cnr.it/shibb/shiblogin.php">
             <div className="btn-garr">
@@ -24,7 +33,9 @@ export default function Index(props) {
             <FormattedMessage {...messages.garrButton} />
             </div>
           </a>
-        </Col>
+        </Col>  
+        }      
+        {spid_login_enabled &&   
         <Col xs="12">
             <div id="my-spid-button"> 
             {/* <div className="btn-garr-container">
@@ -39,7 +50,7 @@ export default function Index(props) {
               Il login tramite SPID richiede che JavaScript sia abilitato nel browser
             </noscript>
           </div>
-        </Col>
+        </Col>}        
       </Row>
     </CardFooter>
   )
