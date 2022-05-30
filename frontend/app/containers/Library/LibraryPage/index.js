b2c8fcf7cf90fb9ab2bec3a835f5e5d9cf8e51e3
Alessandro Tugnoli <a.tugnoli@area.bo.cnr.it>
2022-05-30 09:30:28 +0200

    library "manage_patron" flag (partial implem.)

--- frontend/app/containers/Library/LibraryPage/index.js
+++ frontend/app/containers/Library/LibraryPage/index.js
@@ -13,7 +13,7 @@ import makeSelectLibrary,{isLibraryLoading} from "../selectors";
 import {compose} from "redux";
 import { connect } from 'react-redux';
 import {BasePage} from "components";
-import libraryRoutes from "routes/libraryRoutes";
+import libroutes from "routes/libraryRoutes";
 import {requestGetLibrary} from '../actions'
 import {getAuthResource} from "utils/permissions";
 
@@ -26,31 +26,71 @@ function LibraryPage(props) {
     id: match.params.library_id,
   })
 
-  const [filteredRoutes,setFilteredRoutes]=useState(libraryRoutes)
+  const [filteredRoutes,setFilteredRoutes]=useState(libroutes)
+
+  const [mounted,setIsMounted]=useState(false);
 
   useEffect(() => {
-    if(!isLoading){
+    //if(!isLoading){
       dispatch(requestGetLibrary(match.params.library_id,'titles,departments'))
-    }
+    //}
   }, [match.params.library_id])
 
 
   //filter library dashboard menu/route based on profile type (1=borrow, 2=borrow+lending)
-  useEffect(() => {        
-    if(library.library.profile_type==1) //only borrowing
-    {
-      //remove lending
-      setFilteredRoutes(libraryRoutes.filter( (route)=> {
-        return route.name!="Lending"
-      }));            
-    }    
-  }, [library])
+  useEffect(() => { 
+    console.log("libraryRoutes routes",libroutes)
+    let libraryRoutes=libroutes    
+    
+    console.log("libraryRoutes PRIMA",libraryRoutes)
+      if(library.library.profile_type==1) //only borrowing
+      {
+        //remove lending
+        libraryRoutes=libraryRoutes.filter( (route)=> {        
+          return route.path!="/lending" 
+        });                                     
+      }  
+      
+      /* TODO FIX: this doesn't work after switch to another library and back to another) but we've to implement manage_patron field*/
+      if(library.library.manage_patrons==0) //not manage patrons
+      {      
+        //remove patrons related menu items
+        libraryRoutes=libraryRoutes.filter( (route)=> {
+          return (
+            route.path!="/delivery" &&
+            route.path!="/patrons"
+          )}
+        );
+
+        libraryRoutes.forEach(function(route, index) {
+        console.log("libraryRoutes DENTRO",route)  
+        if (route.path == '/manage' && route.children) {          
+          let route2=route;
+          route2.children=route2.children.filter ( (subroute) => 
+          {
+            return (
+              subroute.path!="/departments" &&
+              subroute.path!="/pickup"              
+            )
+          })
+          libraryRoutes[index]=route2
+        }        
+      }) 
+      console.log("libraryRoutes DOPO",libraryRoutes)                                
+      }
+      
+      setFilteredRoutes(libraryRoutes);            
+      setIsMounted(true)
+    
+  }, [library && library.library.id])
+
+  
 
   
 
   return (
     <>
-      <BasePage {...props} routes={filteredRoutes} messages={messages} resource={resource} />
+      {mounted && <BasePage {...props} routes={filteredRoutes} messages={messages} resource={resource} />}
     </>
   );
 }
