d2ec86cea79809e58c2534423f522d1ccf1affc8
Alessandro Tugnoli <a.tugnoli@area.bo.cnr.it>
2022-06-07 11:45:10 +0200

    devedeploy script

--- startDevDeploy.sh
+++ startDevDeploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Deploy script for DevNilde server: it run all docker container
+# Deploy script for DevNilde server: it run all docker container (NILDE+RSCVD)
 docker-compose -f docker-compose.yml -f docker-compose.devdeploy.yml up -d
