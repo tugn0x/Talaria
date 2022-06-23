b0952bd9f8caaf12997312839a7b7e5e74ccc31a
Alessandro Tugnoli <a.tugnoli@area.bo.cnr.it>
2022-06-23 09:01:12 +0200

    remove useless languages from footer

--- frontend/app/components/Footer/index.js
+++ frontend/app/components/Footer/index.js
@@ -28,12 +28,12 @@ function Footer(props) {
             <NavLink to="#" onClick={(e)=>changeLang('en')}>
               English (UK)
             </NavLink>
-            <NavLink to="#" onClick={(e)=>changeLang('fr')}>
+            {/*<NavLink to="#" onClick={(e)=>changeLang('fr')}>
               Français (France)
             </NavLink>
             <NavLink to="#" onClick={(e)=>changeLang('es')}>
               Español
-            </NavLink>
+            </NavLink>*/}
           </div>
           <div className="footer-menu">
           {props.isLogged && 
