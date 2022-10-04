1f692d4167046b13f5fb6df6d1e284c4a7e6b32b
Rabih Kahaleh <rabih.kahaleh@balamand.edu.lb>
2022-10-04 15:03:27 +0300

    LibReg_Update + Update File_Upload controller, Env file, React. But we must set in manuallt in NGINX

--- app/Http/Controllers/Files/FileUploadDocdelRequestController.php
+++ app/Http/Controllers/Files/FileUploadDocdelRequestController.php
@@ -23,20 +23,31 @@ class FileUploadDocdelRequestController extends ApiController
     public function UploadFile(Request $request)
     {
         try {
+            $file_64 = $request->get("filename"); 
+            $size_in_bytes = (int) (strlen(rtrim($file_64, '=')) * 3 / 4);
+            $size_in_kb    = $size_in_bytes / 1024;
+            $size_in_mb    = $size_in_kb / 1024;
 
-        //if($request->has("filehash"))
-        //    Log::info(print_r( $request->get("q"), true));  
-        $file_64 = $request->get("filename"); 
-        //Log::info(print_r( $file_64, true));  
-        $extension = explode('/', explode(':', substr($file_64, 0, strpos($file_64, ';')))[1])[1];
-        $replace = substr($file_64, 0, strpos($file_64, ',')+1); 
-        $file = str_replace($replace, '', $file_64); 
-        $file = str_replace(' ', '+', $file); 
-        $fileName = Str::random(10).'.'.$extension;
-        Storage::disk('public')->put($fileName, base64_decode($file));
-        } catch (ModelNotFoundException $exception) {
-            return \Response::json(['data' => null, 'status'=>'failed']);
+            $variable = env("MAX_UPLOAD_FILE", "100");
+            Log::info(print_r($variable,true));
+
+                if ($size_in_mb <= $variable)
+                {
+                    $extension = explode('/', explode(':', substr($file_64, 0, strpos($file_64, ';')))[1])[1];
+                    $replace = substr($file_64, 0, strpos($file_64, ',')+1); 
+                    $file = str_replace($replace, '', $file_64); 
+                    $file = str_replace(' ', '+', $file); 
+                    $fileName = Str::random(10).'.'.$extension;
+                    Storage::disk('public')->put($fileName, base64_decode($file));
+                }
+                else
+                    return \Response::json(['data' => null, 'status'=>'The Uploaded File Exceeds the defined upload_max_filesize']);
+
+            } catch (ModelNotFoundException $exception) {
+                return \Response::json(['data' => null, 'status'=>'failed']);
+            }
+            return \Response::json(['data' => $fileName, 'status'=>'uploaded', 'originalfilename'=>$request->get("uploadFile")]);
         }
-        return \Response::json(['data' => $fileName, 'status'=>'uploaded', 'originalfilename'=>$request->get("uploadFile")]);
-    }   
-}
+    }
+        
+
