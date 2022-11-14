76b91fee1e7e6ab19c9c0381cee67765cc47ddec
Alessandro Tugnoli <a.tugnoli@area.bo.cnr.it>
2022-11-14 08:12:25 +0100

    API errors: EN translations

---
+++ resources/lang/en/response.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'create_relation_failed' => 'Create relationship failed',
+    'update_relation_failed'=>'Update relationship failed',
+    'create_failed'=>'Create resource failed',
+    'update_failed'=>'Update resource failed',
+    'delete_failed'=>'Delete resource failed',
+    'destroy_failed'=>'Destroy resource failed',
+    'restore_failed'=>'Restore resource failed',
+    'attach_failed'=>'Attach resource failed',
+    'detach_failed'=>'Detach resource failed',
+    'update_meta_failed'=>'Update metadata failed',
+    
+
+];
