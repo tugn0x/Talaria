26c160c79ae307639c960c5272bb993d26690434
Alessandro Tugnoli <a.tugnoli@area.bo.cnr.it>
2023-02-01 14:56:56 +0100

    Institution seeder: test import from CSV

--- database/seeds/InstitutionsTableSeeder.php
+++ database/seeds/InstitutionsTableSeeder.php
@@ -16,5 +16,22 @@ class InstitutionsTableSeeder extends Seeder
         Institution::create(['name'=>'Università di Aosta','institution_type_id'=>1,'country_id'=>1]);
         Institution::create(['name'=>'CNR - Consiglio Nazionale delle Ricerche','institution_type_id'=>2,'country_id'=>1]);
         //Institution::create(['id'=>8,'name'=>'Università di Bologna','institution_type_id'=>1]);
+
+        /* TODO: 
+        import by CSV ... 
+
+        
+             $table = 'posts';
+        $file = public_path("/seeders/$table".".csv");
+        $records = Helper::import_CSV($file);
+        
+        foreach ($records as $key => $record) {
+            Post::create([
+                'title' => $record['title'],
+                'subtitle' => $record['subtitle'],
+                'description' => $record['description'],
+            ]);
+        }
+        */
     }
 }
