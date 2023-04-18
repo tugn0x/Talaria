722a71fe8ad6a2dd280ce4edf9dafa1f78d97bd3
Alessandro Tugnoli <a.tugnoli@area.bo.cnr.it>
2023-04-18 14:07:11 +0200

    request tag style fix

--- frontend/app/components/Library/RequestTags/index.js
+++ frontend/app/components/Library/RequestTags/index.js
@@ -9,9 +9,9 @@ return (data.length>0 &&
         <div className="request_tags">
                 <span className="tags-row">
                     <i className="fa-solid fa-tag"></i>
-                    {data.map(tag => <span key={tag.id}>
+                    {data.map(tag => <span key={tag.id} className="badge badge-light">
                         {tag.name} 
-                        {removeTag && <i className="fa-solid fa-xmark"  onClick={() => removeTag(tag.id)}></i>}
+                        {removeTag && <i className="fa-solid fa-xmark remove_tag_icon"  onClick={() => removeTag(tag.id)}></i>}
                     </span>)}
                 </span>
         </div>
